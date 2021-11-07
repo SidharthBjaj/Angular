3bf4ffb461538e00304e7dbb18a6fc1104b80b39
Sidharth Bajaj <w0744581@myscc.ca>
2021-11-07 01:31:27 -0400

    Assignment_6

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,5 +1,6 @@
 import { Content } from '../helper-files/content-interface';
 import { Component, OnInit } from '@angular/core';
+import { ContentService } from '../services/content.service';
 
 @Component({
   selector: 'app-content-list',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentListComponent implements OnInit {
   contentList: Content[] = [];
-  constructor() { }
+  constructor(private contentService: ContentService) { }
 
   searchTitle(search:String):void{
     let exist = this.contentList.find(content => content.title.toLowerCase() == search.toLowerCase());
@@ -16,53 +17,8 @@ export class ContentListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.contentList = [
-      {
-        id: 1,
-        author: 'Sidharth',
-        title: 'first',
-        body: 'This is body of content in first array',
-        tags: ['Windsor','St. Clair'],
-        imgUrl: 'https://cdn.pixabay.com/photo/2014/05/21/22/28/old-newspaper-350376_960_720.jpg',
-        type: 'school'
-      }, {
-        id: 2,
-        author: 'Sidharth',
-        title: 'second',
-        body: 'This is body of content in second array',
-        imgUrl: 'https://cdn.pixabay.com/photo/2014/05/21/22/28/old-newspaper-350376_960_720.jpg',
-        tags: ['Windsor','St. Clair'],
-        type: 'windsor'
-      },
-      {
-        id: 3,
-        author: 'Sidharth',
-        title: 'third',
-        body: 'This is body of content in third array',
-        // imgUrl: 'https://cdn.pixabay.com/photo/2014/05/21/22/28/old-newspaper-350376_960_720.jpg',
-        tags: ['Windsor','St. Clair'],
-        type: 'windsor'
-      },
-      {
-        id: 4,
-        author: 'Sidharth',
-        title: 'fourth',
-        body: 'This is body of content in forth array',
-        imgUrl: 'https://cdn.pixabay.com/photo/2014/05/21/22/28/old-newspaper-350376_960_720.jpg',
-        tags: ['Windsor','St. Clair'],
-        type: 'windsor'
-      },
-      {
-        id: 5,
-        author: 'Sidharth',
-        title: 'fifth',
-        body: 'This is body of content in fifth array',
-        imgUrl: 'https://cdn.pixabay.com/photo/2014/05/21/22/28/old-newspaper-350376_960_720.jpg',
-        tags: ['Windsor','St. Clair'],
-        type: 'school'
-      },
-
-    ];
+    this.contentService.getContentsObs().subscribe((content) => {
+      this.contentList = content;
+    });
   }
-
 }
---
+++ src/app/helper-files/contentDb.ts
@@ -0,0 +1,49 @@
+import {Content} from "./content-interface";
+
+export const CONTENT_LIST: Content[] = [
+
+    {
+        id: 1,
+        author: 'Sidharth',
+        title: 'first',
+        body: 'This is body of content in first array',
+        tags: ['Windsor','St. Clair'],
+        imgUrl: 'https://cdn.pixabay.com/photo/2014/05/21/22/28/old-newspaper-350376_960_720.jpg',
+        type: 'school'
+    }, {
+        id: 2,
+        author: 'Sidharth',
+        title: 'second',
+        body: 'This is body of content in second array',
+        imgUrl: 'https://cdn.pixabay.com/photo/2014/05/21/22/28/old-newspaper-350376_960_720.jpg',
+        tags: ['Windsor','St. Clair'],
+        type: 'windsor'
+    },
+    {
+        id: 3,
+        author: 'Sidharth',
+        title: 'third',
+        body: 'This is body of content in third array',
+        // imgUrl: 'https://cdn.pixabay.com/photo/2014/05/21/22/28/old-newspaper-350376_960_720.jpg',
+        tags: ['Windsor','St. Clair'],
+        type: 'windsor'
+    },
+    {
+        id: 4,
+        author: 'Sidharth',
+        title: 'fourth',
+        body: 'This is body of content in forth array',
+        imgUrl: 'https://cdn.pixabay.com/photo/2014/05/21/22/28/old-newspaper-350376_960_720.jpg',
+        tags: ['Windsor','St. Clair'],
+        type: 'windsor'
+    },
+    {
+        id: 5,
+        author: 'Sidharth',
+        title: 'fifth',
+        body: 'This is body of content in fifth array',
+        imgUrl: 'https://cdn.pixabay.com/photo/2014/05/21/22/28/old-newspaper-350376_960_720.jpg',
+        tags: ['Windsor','St. Clair'],
+        type: 'school'
+    },
+]
